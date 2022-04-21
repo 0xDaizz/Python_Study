7360179d22fd6852418043bd71f9dd17de8afc26
Daiz <i7epr@naver.com>
2022-04-21 22:56:24 +0900

    220421 오픈씨예제
    오픈씨에서 price 모아보는 거 만들어보려함.

--- opensea예제.py
+++ opensea예제.py
@@ -1,11 +1,11 @@
-from selenium import webdriver
+#크롬드라이버 구동
 
+from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
-driver.get("https://opensea.io")
-# webdrivermanager를 이용해서 크롬드라이버 셀레니움 구동하는 방법
+opensea = "http://opensea.io"
 
-
+driver.get(opensea)
